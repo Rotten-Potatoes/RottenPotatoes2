b69462cc855194129c3e01ac49d3160dcf189663
Aniruddha Nadkarni <anadkarni30@gatech.edu>
2016-02-16 04:09:44 -0500

    Everything works

--- app/src/main/java/com/example/rahulraina/rottenpotatoes/MainActivity.java
+++ app/src/main/java/com/example/rahulraina/rottenpotatoes/MainActivity.java
@@ -13,21 +13,32 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    //Variables for Login Screen
+    // Variables for Login Screen
     private EditText username;
     private EditText password;
     private TextView profile_button;
 
-    //Variables for Registration Screen
+    // Variables for Registration Screen
     private EditText registerUsername;
     private EditText registerPassword;
     private EditText registerFirstName;
     private EditText registerLastName;
 
-    //HashMap indexed by username
+    // HashMap from username to User Object
     HashMap<String, User> user_holder = new HashMap<>();
 
+    private User currentUser;
 
+    // Lets us know if we have to clear the Login screen password
+    private boolean cameFromLogin = false;
+
+    // Variables for Edit Profile Screen
+    private EditText editUsername;
+    private EditText editPassword;
+    private EditText editFirstName;
+    private EditText editLastName;
+    private EditText editMajor;
+    private EditText editInterests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,56 +46,140 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
     }
 
+
+///////////////////////////////////////////////////////
+
+    // These are the methods for the buttons on the opening Register/Sign In screen
     public void onClickSignIn(View v) {
         setContentView(R.layout.sign_in_screen);
     }
 
+    public void onClickBeginRegistration(View v) {
+        setContentView(R.layout.registration_screen);
+        registerUsername = (EditText) findViewById(R.id.userRegisterUsername);
+        registerPassword = (EditText) findViewById(R.id.userRegisterPassword);
+        registerFirstName = (EditText) findViewById(R.id.userRegisterFirstName);
+        registerLastName = (EditText) findViewById(R.id.userRegisterLastName);
+    }
+
+///////////////////////////////////////////////////////
+
+    // These are the methods for the buttons on the Registration screen
+    public void onClickRegister(View v) {
+        Set usernames = user_holder.keySet();
+        String username = registerUsername.getText().toString();
+        String password = registerPassword.getText().toString();
+        String firstname = registerFirstName.getText().toString();
+        String lastname = registerLastName.getText().toString();
+
+        if (username.equals("") || password.equals("")
+                || firstname.equals("") || lastname.equals("")) {
+            Toast.makeText(MainActivity.this, "One or more fields is missing or invalid", Toast.LENGTH_SHORT).show();
+        } else {
+            boolean isUsernameTaken = usernames.contains(username);
+
+            if (!isUsernameTaken) {
+                //create user
+                User newUser = new User(username, password,
+                        firstname, lastname);
+                user_holder.put(username, newUser);
+                setCurrentUser(username);
+                registerUsername.setText("");
+                registerLastName.setText("");
+                registerFirstName.setText("");
+                registerPassword.setText("");
+                switchToMainApp();
+            } else {
+                Toast.makeText(MainActivity.this, "Username Taken", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void onClickCancelRegister(View v) {
+        registerUsername.setText("");
+        registerLastName.setText("");
+        registerFirstName.setText("");
+        registerPassword.setText("");
+        setContentView(R.layout.activity_main_screen);
+    }
+
+///////////////////////////////////////////////////////
+
+    // These are the methods for the buttons on the Login screen
     public void onClickLogin(View v) {
         username = (EditText) findViewById(R.id.editText);
         password = (EditText) findViewById(R.id.editText2);
 
-        if (username.getText().toString().toLowerCase().equals("user")
-                && password.getText().toString().equals("pass")) {
-            setContentView(R.layout.main_post_sign_in);
-            profile_button = (TextView) findViewById(R.id.edit_profile);
-            String name =  "User Name";
-            profile_button.setText(String.format("%s's Profile", name));
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+
+        if (user_holder.keySet().contains(usernameString)
+                && passwordString.equals(user_holder.get(usernameString).getPassword())) {
+            setCurrentUser(usernameString);
+            cameFromLogin = true;
+            switchToMainApp();
+
         } else {
             password.setText("");
             Toast.makeText(MainActivity.this, "Wrong username or password", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void onClickRegister(View v) {
-        Toast.makeText(MainActivity.this, "Well it got here", Toast.LENGTH_SHORT).show();
-        setContentView(R.layout.registration_screen);
-        registerUsername = (EditText) findViewById(R.id.userRegisterUsername);
-        registerPassword = (EditText) findViewById(R.id.userRegisterPassword);
-        registerFirstName = (EditText) findViewById(R.id.userRegisterFirstName);
-        registerLastName = (EditText) findViewById(R.id.userRegisterLastName);
-
-        Set usernames = user_holder.keySet();
-        boolean isUsernameTaken = usernames.contains(registerUsername.getText().toString());
-
-//        if (usernames.size() == 0 || !isUsernameTaken) {
-//            //create user
-//            User newUser = new User(registerUsername.getText().toString(),
-//                    registerPassword.getText().toString(), registerFirstName.getText().toString(),
-//                    registerLastName.getText().toString());
-//            setContentView(R.layout.main_post_sign_in);
-//        } else {
-//            Toast.makeText(MainActivity.this, "Username Taken", Toast.LENGTH_SHORT).show();
-//        }
-    }
-
-    public void sign_in_back_click(View v) {
+    public void onClickSignInBack(View v) {
         setContentView(R.layout.activity_main_screen);
     }
 
+///////////////////////////////////////////////////////
+
+    // These are the methods for the main app opening screen
     public void onClickSignOut(View v) {
-        username.setText("");
-        password.setText("");
+        if (cameFromLogin) {
+            password.setText("");
+        }
         setContentView(R.layout.activity_main_screen);
+    }
+
+    public void onClickEditProfile(View v) {
+//        editUsername = (EditText) findViewById(R.id.userEditUsername);
+//        editPassword = (EditText) findViewById(R.id.userEditPassword);
+//        editFirstName = (EditText) findViewById(R.id.userEditFirstName);
+//        editLastName = (EditText) findViewById(R.id.userEditLastName);
+//        editMajor = (EditText) findViewById(R.id.userEditMajor);
+//        editInterests = (EditText) findViewById(R.id.userEditInterests);
+//
+//        editUsername.setText(currentUser.getUsername());
+//        editPassword.setText(currentUser.getPassword());
+//        editFirstName.setText(currentUser.getFirstName());
+//        editLastName.setText(currentUser.getLastName());
+//        editMajor.setText(currentUser.getMajor());
+//
+//        if (currentUser.getInterests().equals("")) {
+//            editInterests.setText("Enter your interests here!");
+//        } else {
+//            editInterests.setText(currentUser.getInterests());
+//        }
+
+        setContentView(R.layout.edit_profile);
+
+        editUsername = (EditText) findViewById(R.id.userEditUsername);
+        editPassword = (EditText) findViewById(R.id.userEditPassword);
+        editFirstName = (EditText) findViewById(R.id.userEditFirstName);
+        editLastName = (EditText) findViewById(R.id.userEditLastName);
+        editMajor = (EditText) findViewById(R.id.userEditMajor);
+        editInterests = (EditText) findViewById(R.id.userEditInterests);
+
+        editUsername.setText(currentUser.getUsername());
+        editPassword.setText(currentUser.getPassword());
+        editFirstName.setText(currentUser.getFirstName());
+        editLastName.setText(currentUser.getLastName());
+        editMajor.setText(currentUser.getMajor());
+
+        if (currentUser.getInterests().equals("")) {
+            editInterests.setText("Enter your interests here!");
+        } else {
+            editInterests.setText(currentUser.getInterests());
+        }
+
     }
 
     public void onClickFriends(View v) {
@@ -97,16 +192,75 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "You're not in any groups yet!", Toast.LENGTH_SHORT).show();
     }
 
-    public void onClickProfile(View v) {
-        setContentView(R.layout.edit_profile);
+///////////////////////////////////////////////////////
+
+    // These methods are for the buttons on the update profile screen
+    public void onClickUpdateProfile(View v) {
+        String newUsername = editUsername.getText().toString();
+        String newPassword = editPassword.getText().toString();
+        String newFirstName = editFirstName.getText().toString();
+        String newLastName = editLastName.getText().toString();
+        String newMajor = editMajor.getText().toString();
+        String newInterests = editInterests.getText().toString();
+
+        if (!(newUsername.equals(currentUser.getUsername()))
+                || !(newPassword.equals(currentUser.getPassword()))
+                || !(newFirstName.equals(currentUser.getFirstName()))
+                || !(newLastName.equals(currentUser.getLastName()))
+                || !(newMajor.equals(currentUser.getMajor()))
+                || !(newInterests.equals(currentUser.getInterests()))) {
+
+            if ((newUsername.equals(""))
+                    || (newPassword.equals(""))
+                    || (newFirstName.equals(""))
+                    || (newLastName.equals(""))) {
+
+                Toast.makeText(MainActivity.this, "Invalid field(s). Please check your input!", Toast.LENGTH_SHORT).show();
+            } else {
+                if (!(newUsername.equals(currentUser.getUsername()))) {
+                    if (user_holder.keySet().contains(newUsername)) {
+                        Toast.makeText(MainActivity.this, "Username taken", Toast.LENGTH_SHORT).show();
+                    } else {
+                        user_holder.remove(currentUser.getUsername());
+                        currentUser.updateUser(newUsername, newPassword, newFirstName,
+                                newLastName, newMajor, newInterests);
+                        user_holder.put(currentUser.getUsername(), currentUser);
+                    }
+                } else {
+                    currentUser.updateUser(newUsername, newPassword, newFirstName,
+                            newLastName, newMajor, newInterests);
+                }
+            }
+        } else {
+            switchToMainApp();
+        }
+
     }
 
-    public void onClickCancelRegister(View v) {
-        registerUsername.setText("");
-        registerLastName.setText("");
-        registerFirstName.setText("");
-        registerPassword.setText("");
-
-        setContentView(R.layout.activity_main_screen);
+    public void onClickCancelEdit(View v) {
+        switchToMainApp();
     }
+
+
+
+///////////////////////////////////////////////////////
+
+    // Helper Methods
+
+    // This sets/updates the current user variables
+
+    public void setCurrentUser(String username) {
+        currentUser = user_holder.get(username);
+    }
+
+    // This switches to the main application opening screen
+    // Used by the Login screen and Registration screen
+
+    public void switchToMainApp() {
+        setContentView(R.layout.main_post_sign_in);
+        profile_button = (TextView) findViewById(R.id.edit_profile);
+        profile_button.setText(String.format("%s's Profile", currentUser.getFullName()));
+    }
+
+
 }
--- app/src/main/java/com/example/rahulraina/rottenpotatoes/User.java
+++ app/src/main/java/com/example/rahulraina/rottenpotatoes/User.java
@@ -8,6 +8,8 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    private String major = "";
+    private String interests = "";
 
     public User(String username, String password, String firstName, String lastName) {
         this.username = username;
@@ -15,4 +17,44 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getInterests() {
+        return interests;
+    }
+
+    public void updateUser(String username, String password,
+                           String firstName, String lastName,
+                           String major, String interests) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+        this.interests = interests;
+    }
+
 }
